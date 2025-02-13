2952fe73d2d103ea9fd6258ce185521e32b5a02f
vfuc06 <phucnv.24it@vku.udn.vn>
2025-02-13 16:55:03 +0700

    first commit

---
+++ src/Ex1_2/Circle.java
@@ -0,0 +1,33 @@
+package Ex1_2;
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[radius=" + radius + "]";
+    }
+}
---
+++ src/Ex1_2/TestCircleEx1_2.java
@@ -0,0 +1,16 @@
+package Ex1_2;
+public class TestCircleEx1_2 {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1.1);
+        System.out.println(c1);
+        Circle c2 = new Circle();
+        System.out.println(c2);
+
+        c1.setRadius(2.2);
+        System.out.println(c1);
+        System.out.println("radius is: " + c1.getRadius());
+
+        System.out.printf("area is: %.2f%n", c1.getArea());
+        System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+    }
+}
